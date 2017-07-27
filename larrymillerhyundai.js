3b297df80ab34c57429627b273ae48ad9c38c193
cc-test78 <sd.samara+78@gmail.com>
2017-07-27 18:01:17 +0400

    Update example

---
+++ larrymillerhyundai.js
@@ -0,0 +1,52 @@
+(function() {
+
+    var header = document.querySelector('.header-default');
+    if (!header) {
+        return;
+    }
+
+    function updateButtonPosition(button) {
+        if (window.scrollY > 0) {
+            button.style.top = '5px';
+            button.style.left = (window.innerWidth > 1000) ? '570px' : '540px';
+        } else {
+            button.style.top = '85px';
+            button.style.left = '420px';
+        }
+    }
+
+    function createCarCodeButton() {
+        var button = document.createElement('button');
+        button.className = 'sms-button';
+        button.style.position = 'absolute';
+        button.style.width = '150px';
+        button.style.height = '40px';
+        button.style.borderWidth = '0';
+        button.style.borderRadius = '5px';
+        button.style.outline = 'none';
+        button.style.backgroundColor = '#f3692a';
+        button.style.color = '#fff';
+        button.style.fontSize = '16px';
+        button.innerText = 'Text Us';
+        button.style.cursor = 'pointer';
+        return button;
+    }
+
+    var button = createCarCodeButton();
+    header.appendChild(button);
+    updateButtonPosition(button);
+
+    var widget = new CarcodeWidget();
+    button.addEventListener('click', function(e) {
+        widget.clickHandler(e);
+    });
+
+    window.addEventListener('scroll', function() {
+        updateButtonPosition(button);
+    });
+
+    window.addEventListener('resize', function() {
+        updateButtonPosition(button);
+    });
+
+}());
